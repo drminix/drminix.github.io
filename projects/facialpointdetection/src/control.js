b30d2c5086dbbe9ad79e971024c541c3fa40cee3
sang lee <sang@revealtech.ai>
2020-04-27 23:04:12 -0700

    detects face using opencv

--- projects/facialpointdetection/src/control.js
+++ projects/facialpointdetection/src/control.js
@@ -5,18 +5,27 @@ const height = 400;
 const FPS = 30;
 let stream;
 let streaming = false;
+let faceCascadeFile = 'haarcascade_frontalface_default.xml';
 document.body.classList.add("loading");
 
 function onOpenCvReady() {
   cv.onRuntimeInitialized = onReady;
   document.body.classList.remove("loading");
-  console.log("OpenCV is ready: ",cv)
+  console.log("OpenCV is ready: ",cv);
 }
 
+function meme() {
+    console.log("files are loaded");
+}
 function onReady() {
     let src;
     let dst;
+    let gray;
     const cap = new cv.VideoCapture(video);
+    let classifier;
+    let faces 
+  
+    
     actionBtn.addEventListener('click', () => {
         if (streaming) {
             stop();
@@ -36,7 +45,15 @@ function onReady() {
             video.play();
             streaming = true;
             src = new cv.Mat(height, width, cv.CV_8UC4);
-            dst = new cv.Mat(height, width, cv.CV_8UC1);
+            dst = new cv.Mat(height, width, cv.CV_8UC4);
+            faces = new cv.RectVector();
+            gray = new cv.Mat();
+            // load pre-trained classifiers
+            classifier = new cv.CascadeClassifier();
+            if(classifier.load(faceCascadeFile) != true) {
+                alert("error while loading face detection file");
+            }
+            
             setTimeout(processVideo, 0)
         })
         .catch(err => console.log(`An error occurred: ${err}`));
@@ -57,12 +74,32 @@ function onReady() {
         if (!streaming) {
             src.delete();
             dst.delete();
+            gray.delete();
+            faces.delete();
+            classifier.delete();
             return;
         }
         const begin = Date.now();
-        cap.read(src)
-        cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+        cap.read(src);
+        src.copyTo(dst);
+        cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY,0);
+        
+        // detect faces.
+        classifier.detectMultiScale(gray, faces, 1.1, 3, 0);
+        // draw faces.
+        for (let i = 0; i < faces.size(); ++i) {
+            let face = faces.get(i);
+            let point1 = new cv.Point(face.x, face.y);
+            let point2 = new cv.Point(face.x + face.width, face.y + face.height);
+            cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
+            
+            //run the model
+            
+        }
+        
         cv.imshow('canvas_output', dst);
+        
+        //schedule next call
         const delay = 1000/FPS - (Date.now() - begin);
         setTimeout(processVideo, delay);
     }
